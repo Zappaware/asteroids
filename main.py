7579e08efc90c4dd69e3e6c1a6ce18af8f7c8079
Zappaware <sergio_germanc@hotmail.com>
2025-07-23 21:42:37 -0600

    Modify main file and add circleshape class

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -17,6 +19,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+         
 
 
 
